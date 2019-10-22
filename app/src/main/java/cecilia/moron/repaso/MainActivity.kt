e07e927968f45e1d62528f8e9d6e7c8f41c429e6
CeciliaMoron <cecysju@hotmail.com>
2019-10-21 20:58:46 -0500

    Initial commit

---
+++ app/src/main/java/cecilia/moron/repaso/MainActivity.kt
@@ -0,0 +1,12 @@
+package cecilia.moron.repaso
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
